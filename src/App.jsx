f93ae9accf09d80aa664dda4701d47ff16a8d2b6
yunhu89 <yunhu89@naver.com>
2025-04-10 15:43:59 +0900

    README fule fix

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+//import './App.css'
+
+function App() {
+  return (
+    <div>
+    <h1>안녕하세요 저는 김윤후입니다</h1>
+      <p>앱 웹 프론트개발자를 지망합니다</p>
+      <p>관심 기술: Flutter/RN/TypeScript/React</p>
+      <a href="https://velog.io/@yunhu89"></a>
+  </div>
+  );
+}
+
+export default App;
+
+/* 
+실행하는 코드를 작성할 때는 App.jsx에 중점적으로 코드를 장석함. 
+
+*/
